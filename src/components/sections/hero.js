88e1cfdd002be1c3267506870d71b829d80d1bff
Eliud Ogola <51315397+seveelly@users.noreply.github.com>
2022-05-01 20:18:24 +0300

    Update hero.js
    software

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Eliud Ogola.</h2>;
-  const three = <h3 className="big-heading">I build programs.</h3>;
+  const three = <h3 className="big-heading">I build software.</h3>;
   const four = (
     <>
       <p>
