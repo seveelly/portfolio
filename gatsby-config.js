707927059af599f45c05ada52c30e7f99006e91e
seveelly <eliudonyango98@gmail.com>
2022-05-04 17:48:16 +0300

    gatsbyconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Eliud Ogola is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://Eliudogola.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@eliudogola',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
