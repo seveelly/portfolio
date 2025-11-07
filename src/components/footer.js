e313e9ba6b68dd44728a6892fcedbd46a5e624f9
seveelly <eliudonyango98@gmail.com>
2025-11-07 14:15:11 +0000

    eo

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="#">
-          <div>Designed by bchiang &amp; Built by Eliud Ogola</div>
+          <div>Designed &amp; Built by E.O</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
